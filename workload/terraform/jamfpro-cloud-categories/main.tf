03adcd38ef3e2ff23df7ec7d690d3055699fddba
ShocOne <dafydd.watkins@deploymenttheory.com>
2023-07-27 16:45:49 +0100

    update

--- workload/terraform/jamfpro-cloud-categories/main.tf
+++ workload/terraform/jamfpro-cloud-categories/main.tf
@@ -19,7 +19,7 @@ resource "jamf_category" "terraform-example" {
 #------------------------------ JAMF Pro Buildings ------------------------------------------------#
 #--------------------------------------------------------------------------------------------------#
 resource "jamf_building" "example" {
-  name     = "example"
+  name = "example"
 
   street_address1 = "1-1-1"
   street_address2 = "example Building"
