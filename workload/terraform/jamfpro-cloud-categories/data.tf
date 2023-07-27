c165fdca37ed79214f4614555976ca7f1ef55b1e
ShocOne <dafydd.watkins@deploymenttheory.com>
2023-07-27 14:13:05 +0100

    update

--- workload/terraform/jamfpro-cloud-categories/data.tf
+++ workload/terraform/jamfpro-cloud-categories/data.tf
@@ -1,3 +1,7 @@
 data "jamf_category" "example" {
   name = "example"
 }
+
+data "jamf_category" "terraform" {
+  name = "erraform"
+}
--- workload/terraform/jamfpro-cloud-categories/main.tf
+++ workload/terraform/jamfpro-cloud-categories/main.tf
@@ -2,3 +2,8 @@ resource "jamf_category" "example" {
   name     = "example"
   priority = 9
 }
+
+resource "jamf_category" "terraform" {
+  name     = "terraform"
+  priority = 9
+}
