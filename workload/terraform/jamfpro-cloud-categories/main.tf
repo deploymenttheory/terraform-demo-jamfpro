26578ceae11451ef9c2fcd0850ab605e86446f48
ShocOne <dafydd.watkins@deploymenttheory.com>
2023-07-27 16:49:11 +0100

    update

--- workload/terraform/jamfpro-cloud-categories/main.tf
+++ workload/terraform/jamfpro-cloud-categories/main.tf
@@ -1,12 +1,14 @@
 #------------------------------ JAMF Pro Categories -----------------------------------------------#
-# If resource doesn't exist it will be created
-# If resource gets a new name it will remove the previous resouce and create a new one
-# If the resource attribute is changed, then will keep the resource but update it's attribute.
-# If resource is removed then it will be removed by Terraform
-# If resource with the same name already exists, will throw duplicate field error and fail
+# Behaviour:
+# - If resource doesn't exist it will be created
+# - If resource gets a new name it will remove the previous resouce and create a new one
+# - If the resource attribute is changed, then will keep the resource but update it's attribute.
+# - If resource is removed then it will be removed by Terraform
+# - If resource with the same name already exists, will throw duplicate field error and fail
 #
+# RBAC:
 # Service account requires the following permissions for CRUD operations respectively
-# Jamf Pro Server Objects - Create / Read / Update / Delete
+# - Jamf Pro Server Objects - Create / Read / Update / Delete
 #--------------------------------------------------------------------------------------------------#
 resource "jamf_category" "Security" {
   name     = "Security"
@@ -18,11 +20,10 @@ resource "jamf_category" "terraform-example" {
   priority = 4
 }
 
-
 #------------------------------ JAMF Pro Buildings ------------------------------------------------#
 #--------------------------------------------------------------------------------------------------#
-resource "jamf_building" "example" {
-  name = "example"
+resource "jamf_building" "example2" {
+  name = "example2"
 
   street_address1 = "1-1-1"
   street_address2 = "example Building"
