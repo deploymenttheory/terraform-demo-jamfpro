7ccff59a1a0f83897f90169fea025e8c29efef6d
ShocOne <dafydd.watkins@deploymenttheory.com>
2023-07-27 15:02:39 +0100

    update

--- workload/terraform/jamfpro-cloud-categories/main.tf
+++ workload/terraform/jamfpro-cloud-categories/main.tf
@@ -1,9 +1,9 @@
 resource "jamf_category" "terraform-example-update" {
   name     = "terraform-example-update"
-  priority = 9
+  priority = 3
 }
 
 resource "jamf_category" "terraform-example" {
   name     = "terraform-example"
-  priority = 7
+  priority = 4
 }
