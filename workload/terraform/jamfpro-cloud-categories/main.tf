d527deac8f09112fb39b782902b552f29ce08008
ShocOne <dafydd.watkins@deploymenttheory.com>
2023-07-27 16:53:13 +0100

    update

--- workload/terraform/jamfpro-cloud-categories/main.tf
+++ workload/terraform/jamfpro-cloud-categories/main.tf
@@ -1,15 +1,15 @@
-#------------------------------ JAMF Pro Categories -----------------------------------------------#
-# Behaviour:
-# - If resource doesn't exist it will be created
-# - If resource gets a new name it will remove the previous resouce and create a new one
-# - If the resource attribute is changed, then will keep the resource but update it's attribute.
-# - If resource is removed then it will be removed by Terraform
-# - If resource with the same name already exists, will throw duplicate field error and fail
-#
-# RBAC:
-# Service account requires the following permissions for CRUD operations respectively
-# - Jamf Pro Server Objects - Create / Read / Update / Delete
-#--------------------------------------------------------------------------------------------------#
+#------------------------------ JAMF Pro Categories ------------------------------------------------#
+# Behaviour:                                                                                        #
+# - If resource doesn't exist it will be created.                                                   #
+# - If resource gets a new name it will remove the resouce and create a new one with the new name.  #
+# - If the resource attribute is changed, then will keep the resource but update it's attribute.    #
+# - If resource is removed from the code then it will be removed by Terraform.                      #
+# - If resource with the same name already exists, will throw duplicate field error and fail.       #
+#                                                                                                   #
+# RBAC:                                                                                             #
+# Service account requires the following permissions for CRUD operations respectively.              #
+# - Jamf Pro Server Objects - Create / Read / Update / Delete                                       #
+#---------------------------------------------------------------------------------------------------#
 resource "jamf_category" "Security" {
   name     = "Security"
   priority = 3
@@ -20,8 +20,18 @@ resource "jamf_category" "terraform-example" {
   priority = 4
 }
 
-#------------------------------ JAMF Pro Buildings ------------------------------------------------#
-#--------------------------------------------------------------------------------------------------#
+#------------------------------ JAMF Pro Buildings -------------------------------------------------#
+# Behaviour:                                                                                        #
+# - If resource doesn't exist it will be created.                                                   #
+# - If resource gets a new name it will remove the resouce and create a new one with the new name.  #
+# - If the resource attribute is changed, then will keep the resource but update it's attribute.    #
+# - If resource is removed from the code then it will be removed by Terraform.                      #
+# - If resource with the same name already exists, will throw duplicate field error and fail.       #
+#                                                                                                   #
+# RBAC:                                                                                             #
+# Service account requires the following permissions for CRUD operations respectively.              #
+# - Jamf Pro Server Objects - Create / Read / Update / Delete                                       #
+#---------------------------------------------------------------------------------------------------#
 resource "jamf_building" "example2" {
   name = "example2"
 
@@ -31,4 +41,24 @@ resource "jamf_building" "example2" {
   state_province  = "Tokyo"
   zip_postal_code = "111-1111"
   country         = "Japan"
+}
+
+resource "jamf_building" "example3" {
+  name = "example3"
+
+  street_address1 = "1-1-1"
+  street_address2 = "example Building"
+  city            = "Shibuya-ku"
+  zip_postal_code = "111-1111"
+  country         = "Japan"
+}
+
+resource "jamf_building" "example4" {
+  name = "example4"
+
+  street_address1 = "1-1-1"
+  street_address2 = "example Building"
+  city            = "Shibuya-ku"
+  state_province  = "Tokyo"
+  zip_postal_code = "111-1111"
 }
