985e82b979ae0caa4417cdcf74a018280fc06a73
ShocOne <dafydd.watkins@deploymenttheory.com>
2023-07-27 16:37:42 +0100

    update

--- workload/terraform/jamfpro-cloud-categories/main.tf
+++ workload/terraform/jamfpro-cloud-categories/main.tf
@@ -1,4 +1,10 @@
-#---------------- JAMF Pro Categories ----------------#
+#------------------------------ JAMF Pro Categories -----------------------------------------------#
+# If resource doesn't exist it will be created
+# If resource gets a new name it will remove the previous resouce and create a new one
+# If the resource attribute is changed, then will keep the resource but update it's attribute.
+# If resource is removed then it will be removed by Terraform
+# If resource with the same name already exists, will throw duplicate field error and fail
+#--------------------------------------------------------------------------------------------------#
 resource "jamf_category" "Security" {
   name     = "Security"
   priority = 3
