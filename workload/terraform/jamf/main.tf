5631490641e5def6268de37787116eec9eb964ff
ShocOne <dafydd.watkins@deploymenttheory.com>
2023-07-31 11:21:53 +0100

    update

--- workload/terraform/jamf/main.tf
+++ workload/terraform/jamf/main.tf
@@ -2,15 +2,5 @@
 ##  Create Categories for JAMF PRO                                                              ##
 ##----------------------------------------------------------------------------------------------##
 
-resource "jamf" "building_example" {
-  name = "example1"
-
-  street_address1 = "1-1-1"
-  street_address2 = "example Building"
-  city            = "Shibuya-ku"
-  state_province  = "Tokyo"
-  zip_postal_code = "111-1111"
-  country         = "Japan"
-}
 
 #------------------------------ JAMF Pro Computer Extension Attribute -----------------------------#
