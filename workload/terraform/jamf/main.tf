c2baaf23d782ff21d77ce3d89f0dd4d5dc61046e
ShocOne <dafydd.watkins@deploymenttheory.com>
2023-07-31 11:22:14 +0100

    update

--- workload/terraform/jamf/main.tf
+++ workload/terraform/jamf/main.tf
@@ -2,5 +2,8 @@
 ##  Create Categories for JAMF PRO                                                              ##
 ##----------------------------------------------------------------------------------------------##
 
-
+module "jamf" {
+  source  = "app.terraform.io/deploymenttheory/jamf/provider"
+  version = "0.0.1"
+}
 #------------------------------ JAMF Pro Computer Extension Attribute -----------------------------#
