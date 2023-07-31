79c1e82285901caa938f04928bbf0c85acb68a1a
ShocOne <dafydd.watkins@deploymenttheory.com>
2023-07-31 11:21:20 +0100

    update

--- workload/terraform/jamf/terraform.tf
+++ workload/terraform/jamf/terraform.tf
@@ -2,9 +2,10 @@ terraform {
   required_version = ">= 1.5.1"
   backend "azurerm" {}
   # Private Registry tf provider
+  # <HOSTNAME>/<ORGANIZATION>/<MODULE_NAME>/<PROVIDER_NAME>
   required_providers {
     jamf = {
-      source  = "deploymenttheory/jamf"
+      source  = "app.terraform.io/deploymenttheory/jamf"
       version = "0.0.1"
     }
   }
