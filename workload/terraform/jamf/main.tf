f5d1b5a4ff04a93e3f63fae2d3e480f91199d5fe
ShocOne <dafydd.watkins@deploymenttheory.com>
2023-07-31 11:03:22 +0100

    update

--- workload/terraform/jamf/main.tf
+++ workload/terraform/jamf/main.tf
@@ -2,19 +2,15 @@
 ##  Create Categories for JAMF PRO                                                              ##
 ##----------------------------------------------------------------------------------------------##
 
-## Modules
-module "jamf" {
-  source  = "app.terraform.io/deploymenttheory/jamf/provider"
-  version = "0.0.1"
+resource "building" "building_example" {
+  name = "example1"
 
-  categories = [
-    {
-      street_address1 = "1-1-1"
-      street_address2 = "example Building"
-      city            = "Shibuya-ku"
-      state_province  = "Tokyo"
-      zip_postal_code = "111-1111"
-      country         = "Japan"
-    },
-  ]
+  street_address1 = "1-1-1"
+  street_address2 = "example Building"
+  city            = "Shibuya-ku"
+  state_province  = "Tokyo"
+  zip_postal_code = "111-1111"
+  country         = "Japan"
 }
+
+#------------------------------ JAMF Pro Computer Extension Attribute -----------------------------#
--- workload/terraform/jamf/terraform.tf
+++ workload/terraform/jamf/terraform.tf
@@ -1,6 +1,7 @@
 terraform {
   required_version = ">= 1.5.1"
   backend "azurerm" {}
+  # Private Registry tf provider
   required_providers {
     jamf = {
       source  = "deploymenttheory/jamf"
