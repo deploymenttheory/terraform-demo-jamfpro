e33513309ed60785559ed83995cba6d7ee490e6f
admin_d.watkins@deploymenttheory.com <admin_d.watkins@deploymenttheory.com>
2023-08-03 07:26:09 +0100

    update

--- workload/terraform/jamfpro/data.tf
+++ workload/terraform/jamfpro/data.tf
@@ -1,13 +1,4 @@
 #---------------- JAMF Pro Buildings ---------------------------#
 
-data "jamfpro_building" "example" {
-  name = "example"
-}
 
 #---------------- JAMF Pro Categories --------------------------#
-data "jamfpro_category" "terraform-example" {
-  name = "terraform-example"
-}
-data "jamfpro_category" "Security" {
-  name = "Security"
-}
