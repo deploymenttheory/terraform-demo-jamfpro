d3e809873d4d966333a09ccf5f3817ec40530280
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-03-08 08:27:42 +0000

    Refactor disk encryption configurations in JamfPro

--- workload/terraform/jamfpro/diskencryptionconfigurations.tf
+++ workload/terraform/jamfpro/diskencryptionconfigurations.tf
@@ -1,47 +1,47 @@
 
-# // jamfpro Institutional Recovery Key config tf example 
+// jamfpro Institutional Recovery Key config tf example 
 
-# resource "jamfpro_disk_encryption_configuration" "jamfpro_disk_encryption_configuration_001" {
-#   name                     = "tf-ghatest-diskencryption-institutional_recovery_key"
-#   key_type                 = "Institutional"      # Or "Individual and Institutional"
-#   file_vault_enabled_users = "Management Account" # Or "Current or Next User"
+resource "jamfpro_disk_encryption_configuration" "jamfpro_disk_encryption_configuration_001" {
+  name                     = "tf-ghatest-diskencryption-institutional_recovery_key"
+  key_type                 = "Institutional"      # Or "Individual and Institutional"
+  file_vault_enabled_users = "Management Account" # Or "Current or Next User"
 
-#   institutional_recovery_key {
-#     certificate_type = "PKCS12" # For .p12 certificate types
-#     password         = "secretThing"
-#     data             = filebase64("${path.module}/support_files/filevaultcertificate/FileVaultMaster-sdk.p12")
-#   }
-# }
+  institutional_recovery_key {
+    certificate_type = "PKCS12" # For .p12 certificate types
+    password         = "secretThing"
+    data             = filebase64("${path.module}/support_files/filevaultcertificate/FileVaultMaster-sdk.p12")
+  }
+}
 
-# data "jamfpro_disk_encryption_configuration" "jamfpro_disk_encryption_configuration_001_data" {
-#   id = jamfpro_disk_encryption_configuration.jamfpro_disk_encryption_configuration_001.id
-# }
+data "jamfpro_disk_encryption_configuration" "jamfpro_disk_encryption_configuration_001_data" {
+  id = jamfpro_disk_encryption_configuration.jamfpro_disk_encryption_configuration_001.id
+}
 
-# output "jamfpro_disk_encryption_configuration_001_id" {
-#   value = data.jamfpro_disk_encryption_configuration.jamfpro_disk_encryption_configuration_001_data.id
-# }
+output "jamfpro_disk_encryption_configuration_001_id" {
+  value = data.jamfpro_disk_encryption_configuration.jamfpro_disk_encryption_configuration_001_data.id
+}
 
-# output "jamfpro_disk_encryption_configuration_001_name" {
-#   value = data.jamfpro_disk_encryption_configuration.jamfpro_disk_encryption_configuration_001_data.name
-# }
+output "jamfpro_disk_encryption_configuration_001_name" {
+  value = data.jamfpro_disk_encryption_configuration.jamfpro_disk_encryption_configuration_001_data.name
+}
 
-#  jamfpro Individual Recovery Key config tf example 
+// jamfpro Individual Recovery Key config tf example 
 
-# resource "jamfpro_disk_encryption_configuration" "jamfpro_disk_encryption_configuration_002" {
-#   name                     = "tf-ghatest-diskencryption-individual_recovery_key"
-#   key_type                 = "Individual"
-#   file_vault_enabled_users = "Management Account" # Or "Current or Next User"
+resource "jamfpro_disk_encryption_configuration" "jamfpro_disk_encryption_configuration_002" {
+  name                     = "tf-ghatest-diskencryption-individual_recovery_key"
+  key_type                 = "Individual"
+  file_vault_enabled_users = "Management Account" # Or "Current or Next User"
 
-# }
+}
 
-# data "jamfpro_disk_encryption_configuration" "jamfpro_disk_encryption_configuration_002_data" {
-#   id = jamfpro_disk_encryption_configuration.jamfpro_disk_encryption_configuration_002.id
-# }
+data "jamfpro_disk_encryption_configuration" "jamfpro_disk_encryption_configuration_002_data" {
+  id = jamfpro_disk_encryption_configuration.jamfpro_disk_encryption_configuration_002.id
+}
 
-# output "jamfpro_disk_encryption_configuration_002_id" {
-#   value = data.jamfpro_disk_encryption_configuration.jamfpro_disk_encryption_configuration_002_data.id
-# }
+output "jamfpro_disk_encryption_configuration_002_id" {
+  value = data.jamfpro_disk_encryption_configuration.jamfpro_disk_encryption_configuration_002_data.id
+}
 
-# output "jamfpro_disk_encryption_configuration_002_name" {
-#   value = data.jamfpro_disk_encryption_configuration.jamfpro_disk_encryption_configuration_002_data.name
-# }
+output "jamfpro_disk_encryption_configuration_002_name" {
+  value = data.jamfpro_disk_encryption_configuration.jamfpro_disk_encryption_configuration_002_data.name
+}
