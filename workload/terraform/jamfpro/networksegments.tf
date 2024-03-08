f28d870459f6c84fc02d067c84fecfa3f2588549
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-03-08 10:11:48 +0000

    Refactor code for improved performance

---
+++ workload/terraform/jamfpro/networksegments.tf
@@ -0,0 +1,25 @@
+resource "jamfpro_network_segment" "jamfpro_network_segment_001" {
+  name                 = "Example Network Segment"
+  starting_address     = "192.168.1.1"
+  ending_address       = "192.168.1.254"
+  distribution_server  = "Example Distribution Server"
+  distribution_point   = "Example Distribution Point"
+  url                  = "http://example.com"
+  swu_server           = "Example SWU Server"
+  building             = "Main Building"
+  department           = "IT Department"
+  override_buildings   = true
+  override_departments = false
+}
+
+data "jamfpro_network_segment" "jamfpro_network_segment_001_data" {
+  id = jamfpro_network_segment.jamfpro_network_segment_001.id
+}
+
+output "jamfpro_network_segment_001_id" {
+  value = data.jamfpro_network_segment.jamfpro_network_segment_001_data.id
+}
+
+output "jamfpro_network_segment_001_name" {
+  value = data.jamfpro_network_segment.jamfpro_network_segment_001_data.name
+}
