e9e74f4dda18ea543896c6a3201aa15d7aed2e6b
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-03-08 10:28:17 +0000

    Update dock item data references

--- workload/terraform/jamfpro/dockitems.tf
+++ workload/terraform/jamfpro/dockitems.tf
@@ -21,37 +21,37 @@ resource "jamfpro_dock_item" "jamfpro_dock_item_003" {
 }
 
 data "jamfpro_dock_item" "jamfpro_dock_item_001_data" {
-  id = jamfpro_dock_items.jamfpro_dock_item_001.id
+  id = jamfpro_dock_item.jamfpro_dock_item_001.id
 }
 
 output "jamfpro_dock_item_001_id" {
-  value = data.jamfpro_dock_items.jamfpro_dock_item_001_data.id
+  value = data.jamfpro_dock_item.jamfpro_dock_item_001_data.id
 }
 
 output "jamfpro_dock_item_001_name" {
-  value = data.jamfpro_dock_items.jamfpro_dock_item_001_data.name
+  value = data.jamfpro_dock_item.jamfpro_dock_item_001_data.name
 }
 
 data "jamfpro_dock_item" "jamfpro_dock_item_002_data" {
-  id = jamfpro_dock_items.jamfpro_dock_item_002.id
+  id = jamfpro_dock_item.jamfpro_dock_item_002.id
 }
 
 output "jamfpro_dock_item_002_id" {
-  value = data.jamfpro_dock_items.jamfpro_dock_item_002_data.id
+  value = data.jamfpro_dock_item.jamfpro_dock_item_002_data.id
 }
 
 output "jamfpro_dock_item_002_name" {
-  value = data.jamfpro_dock_items.jamfpro_dock_item_002_data.name
+  value = data.jamfpro_dock_item.jamfpro_dock_item_002_data.name
 }
 
 data "jamfpro_dock_item" "jamfpro_dock_item_003_data" {
-  id = jamfpro_dock_items.jamfpro_dock_item_003.id
+  id = jamfpro_dock_item.jamfpro_dock_item_003.id
 }
 
 output "jamfpro_dock_item_003_id" {
-  value = data.jamfpro_dock_items.jamfpro_dock_item_003_data.id
+  value = data.jamfpro_dock_item.jamfpro_dock_item_003_data.id
 }
 
 output "jamfpro_dock_item_003_name" {
-  value = data.jamfpro_dock_items.jamfpro_dock_item_003_data.name
+  value = data.jamfpro_dock_item.jamfpro_dock_item_003_data.name
 }
