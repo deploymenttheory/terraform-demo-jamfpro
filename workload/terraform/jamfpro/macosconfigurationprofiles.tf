26faa578aae9d5b0b61646dd797cb15942520700
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-03-12 15:23:12 +0000

    Update file path for macOS configuration profile

--- workload/terraform/jamfpro/macosconfigurationprofiles.tf
+++ workload/terraform/jamfpro/macosconfigurationprofiles.tf
@@ -2,7 +2,7 @@
 resource "jamfpro_macos_configuration_profile" "jamfpro_macos_configuration_profile_001" {
   name                = "tf-ghatest-macosconfigprofile-accessibility-options"
   distribution_method = "Install Automatically"
-  payload             = file("${path.module}/support_files/accessibility-chara-nosub-test.mobileconfig")
+  payload             = file("${path.module}/support_files/configuration_profiles/accessibility-chara-nosub-test.mobileconfig")
   category {
     id = -1
   }
