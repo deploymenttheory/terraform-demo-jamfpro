b851203ee8e64483903e03b5fdc643dc7eecb5ed
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-03-13 16:48:51 +0000

    Update category in jamfpro_package_002

--- workload/terraform/jamfpro/packages.tf
+++ workload/terraform/jamfpro/packages.tf
@@ -3,7 +3,7 @@
 resource "jamfpro_package" "jamfpro_package_002" {
   name                 = "tf-ghatest-package-suspiciouspackage"
   package_file_path    = "support_files/packages/SuspiciousPackage.dmg"
-  category             = ""
+  category             = "Unknown"
   info                 = "tf package deployment for demonstration"
   notes                = "This package is used for Terraform provider documentation example."
   priority             = 10
