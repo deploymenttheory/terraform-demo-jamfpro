ea7d9a398f7265384ee9408814776ddcb205fdf4
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-04-04 12:57:25 +0100

    Add refactor option to pull request templates

--- workload/terraform/jamfpro/accounts.tf
+++ workload/terraform/jamfpro/accounts.tf
@@ -1,19 +1,19 @@
-# resource "jamfpro_account" "jamfpro_account_001" {
-#   name                  = "tf-ghatest-account-johnny-appleseed"
-#   directory_user        = false
-#   full_name             = "Jonny Appleseed"
-#   password              = "mySecretThing10"
-#   email                 = "exampleEmailthing@domain.com"
-#   enabled               = "Enabled"
-#   force_password_change = true
-#   access_level          = "Group Access" // Full Access / Site Access / Group Access
-#   privilege_set         = "Custom"       // Custom
+resource "jamfpro_account" "jamfpro_account_001" {
+  name                  = "tf-ghatest-account-johnny-appleseed"
+  directory_user        = false
+  full_name             = "Jonny Appleseed"
+  password              = "mySecretThing10"
+  email                 = "exampleEmailthing@domain.com"
+  enabled               = "Enabled"
+  force_password_change = true
+  access_level          = "Group Access" // Full Access / Site Access / Group Access
+  privilege_set         = "Custom"       // Custom
 
-#   // Use Terraform reference for group name
-#   groups {
-#     name = jamfpro_account_group.jamfpro_account_group_001.name
-#   }
-# }
+  // Use Terraform reference for group name
+  groups {
+    name = jamfpro_account_group.jamfpro_account_group_001.name
+  }
+}
 
 # data "jamfpro_account" "jamfpro_account_001_data" {
 #   id = jamfpro_account.jamfpro_account_001.id
