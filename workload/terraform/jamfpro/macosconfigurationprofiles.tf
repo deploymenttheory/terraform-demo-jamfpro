c6dc4847dfa20f3474dbb35e020adc0bccacce0b
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-04-16 14:00:29 +0100

    Refactor macOS configuration profiles and packages, and update Jamf Pro provider version to 0.0.50

--- workload/terraform/jamfpro/macosconfigurationprofiles.tf
+++ workload/terraform/jamfpro/macosconfigurationprofiles.tf
@@ -6,15 +6,4 @@ resource "jamfpro_macos_configuration_profile" "jamfpro_macos_configuration_prof
   category {
     id = -1
   }
-  scope {
-    all_computers      = false
-    computer_ids       = sort([17, 18])
-    computer_group_ids = sort([53])
-    jss_user_ids       = [4]
-    jss_user_group_ids = [4]
-
-    exclusions {
-      department_ids = [37287]
-    }
-  }
 }
