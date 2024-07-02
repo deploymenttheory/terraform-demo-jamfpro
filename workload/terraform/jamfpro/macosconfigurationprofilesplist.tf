1176a59a6f8d66f24766c0638dbca4132d5c97cf
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-07-02 06:26:42 +0100

    Refactor Jamf Pro configuration files and update macOS configuration profile

--- workload/terraform/jamfpro/macosconfigurationprofilesplist.tf
+++ workload/terraform/jamfpro/macosconfigurationprofilesplist.tf
@@ -1,12 +1,13 @@
 
 resource "jamfpro_macos_configuration_profile_plist" "jamfpro_macos_configuration_profile_001" {
   name                = "tf-ghatest-macosconfigprofile-accessibility-options"
-  distribution_method = "Install Automatically"
-  payload             = file("${path.module}/support_files/configuration_profiles/accessibility-chara-nosub-test.mobileconfig")
+  distribution_method = "Install Automatically" // "Make Available in Self Service", "Install Automatically"
+  payloads            = file("${path.module}/support_files/configuration_profiles/accessibility-chara-nosub-test.mobileconfig")
   category_id         = -1
-
+  user_removable      = false
+  level               = "User"
   scope {
     all_computers = true
-
+    all_jss_users = true
   }
 }
