7b2c0f89ef0215bbb8e977152c6cbfdf938f2207
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-07-02 09:16:26 +0100

    Refactor Jamf Pro configuration files and update jamfpro_auth_method default value to "oauth2"

--- workload/terraform/jamfpro/provider.tf
+++ workload/terraform/jamfpro/provider.tf
@@ -33,7 +33,7 @@ variable "jamfpro_instance_fqdn" {
 variable "jamfpro_auth_method" {
   description = "Auth method chosen for Jamf. Options are 'basic' or 'oauth2'."
   sensitive   = true
-  default     = ""
+  default     = "oauth2"
 }
 
 variable "jamfpro_client_id" {
