92a7451e3f1c706c7172001ced4be8543b04be62
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-07-02 07:31:28 +0100

    modified:   workload/terraform/jamfpro/staticcomputergroups.tf

--- workload/terraform/jamfpro/staticcomputergroups.tf
+++ workload/terraform/jamfpro/staticcomputergroups.tf
@@ -2,7 +2,7 @@ resource "jamfpro_static_computer_group" "static_computer_group_002" {
   name = "tf-ghatest-staticcomputergroup"
 
   // optional
-  site_id = -1
+  //site_id = -1
 
   assignments {
     computer_ids = [16, 17, 20]
