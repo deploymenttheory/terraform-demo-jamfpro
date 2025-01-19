d63745e428559f0a8a8602a2b9faadf393c95509
adtejaa <dharmatejapallapu2002@gmail.com>
2025-01-19 23:36:52 +0530

    adding sites

--- workload/terraform/jamfpro/sites.tf
+++ workload/terraform/jamfpro/sites.tf
@@ -1,10 +1,10 @@
-# resource "jamfpro_site" "jamfpro_site_001" {
-#   name = "tf-demo-site-uk"
-# }
+resource "jamfpro_site" "jamfpro_site_001" {
+  name = "India"
+}
 
-# resource "jamfpro_site" "jamfpro_site_002" {
-#   name = "tf-demo-site-india"
-# }
+resource "jamfpro_site" "jamfpro_site_002" {
+  name = "tf-demo-site-india"
+}
 
 # resource "jamfpro_site" "jamfpro_site_003" {
 #   name = "tf-demo-site-usa"
