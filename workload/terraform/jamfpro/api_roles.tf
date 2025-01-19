7e2287b7ff00ba296ee5ad5da1e90c46bcc2c4f9
adtejaa <dharmatejapallapu2002@gmail.com>
2025-01-20 00:57:01 +0530

    Update api_roles.tf

--- workload/terraform/jamfpro/api_roles.tf
+++ workload/terraform/jamfpro/api_roles.tf
@@ -1,8 +1,9 @@
 
 // cicd jamf api roles with no priviledge as they will be applied dynamically 
 resource "jamfpro_api_role" "jamfpro_api_role_001" {
-  
+   display_name = "tf-demo-all-jamf-pro-privileges-11.7"
 }
 
 resource "jamfpro_api_role" "jamfpro_api_role_002" {
+display_name = "tf-demo-all-jamf-pro-privileges-11.9.1"
  }
