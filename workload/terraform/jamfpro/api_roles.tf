ca955c9658724f1fb5bf62dfd610594db209837c
adtejaa <dharmatejapallapu2002@gmail.com>
2025-01-20 00:58:30 +0530

    Update api_roles.tf

--- workload/terraform/jamfpro/api_roles.tf
+++ workload/terraform/jamfpro/api_roles.tf
@@ -2,8 +2,12 @@
 // cicd jamf api roles with no priviledge as they will be applied dynamically 
 resource "jamfpro_api_role" "jamfpro_api_role_001" {
    display_name = "tf-demo-all-jamf-pro-privileges-11.7"
+privileges = ["Allow User to Enroll",
+    "Assign Users to Computers",
+    "Assign Users to Mobile Devices",
+    "CLEAR_TEACHER_PROFILE_PRIVILEGE",
+    "Change Password",
+    "Create API Integrations",
+    "Create API Roles",
+    "Create Accounts"]
 }
-
-resource "jamfpro_api_role" "jamfpro_api_role_002" {
-display_name = "tf-demo-all-jamf-pro-privileges-11.9.1"
- }
